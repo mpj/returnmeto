6ce000c0243a8732ea0f46e40d5ddc44f18233f7
Mattias Johansson <mpj@mpj.me>
2019-03-15 11:32:39 +0100

    did stuff

--- src/App.js
+++ src/App.js
@@ -3,23 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  reportLocationClicked(event) {
+    event.preventDefault()
+    navigator
+      .geolocation
+      .getCurrentPosition(position => 
+        console.log(
+          "lat lon",
+          position.coords.latitude, 
+          position.coords.longitude
+        )
+      )
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>Hello. I am lost. Help me be found by reporting
+        my location to my owner. 
+        <form>
+          <label for="email">Email (optional, if you want a reward)</label>
+          <input id="email" value="" />
+          <button 
+            onClick={this.reportLocationClicked.bind(this)}>
+              Report location
+          </button>
+        </form>
       </div>
     );
   }
